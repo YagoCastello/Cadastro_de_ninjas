910f02e2dbb275a94fbe65f217addc5d1cb4c886
Yago Castello <yagodealmeidacs@gmail.com>
2025-09-16 23:18:59 -0300

    Utilizei o JPA e criei uma entidade a partir do ninja model

---
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,26 @@
+package dev.java10x.CadastroDeNinjas;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+}
